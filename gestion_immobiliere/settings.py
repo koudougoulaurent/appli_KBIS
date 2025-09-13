a7cc0e11c6946fb613bfdee61eb5a2e17a8883b4
koudougoulaurent <koudougoulaurent7@gmail.com>
2025-09-13 02:35:29 +0200

    Fix ALLOWED_HOSTS pour domaine Render appli-kbis.onrender.com

--- gestion_immobiliere/settings.py
+++ gestion_immobiliere/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ics4n+vw1)3tlekunwt5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver,appli-kbis.onrender.com,.onrender.com').split(',')
 
 
 # Application definition
