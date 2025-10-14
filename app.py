969802b475225ee5ae47edb2743c4f66a6a116d3
koudougoulaurent <koudougoulaurent7@gmail.com>
2025-10-14 16:23:00 +0200

    fix: Force DJANGO_SETTINGS_MODULE dans app.py - Utilise os.environ['DJANGO_SETTINGS_MODULE'] au lieu de setdefault - Force l'utilisation de settings.py - Solution dÃ©finitive pour Render

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import django
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_immobiliere.settings')
+# Force le bon settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'gestion_immobiliere.settings'
 django.setup()
 
 app = get_wsgi_application()
