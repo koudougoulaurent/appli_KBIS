211c29c66e758b2f652c140657b2a6e575e30c4e
koudougoulaurent <koudougoulaurent7@gmail.com>
2025-10-17 14:25:35 +0200

    Fix migration dependencies after removing problematic migrations

--- paiements/migrations/0045_avance_loyer_system.py
+++ paiements/migrations/0045_avance_loyer_system.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('contrats', '0001_initial'),
-        ('paiements', '0044_add_created_updated_at_fields'),
+        ('paiements', '0036_auto_20251004_1310'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
