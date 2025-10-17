af2f6bb0ca3b6357ffaaffc708402554ffa32110
koudougoulaurent <koudougoulaurent7@gmail.com>
2025-10-17 16:30:08 +0200

    RÃ©organiser complÃ¨tement le formulaire d'unitÃ©s locatives par Ã©tapes - Interface claire et logique

--- proprietes/views_unites.py
+++ proprietes/views_unites.py
@@ -198,7 +198,7 @@ def unite_create(request, propriete_id=None):
         'from_property': request.GET.get('from_property') == '1',
     }
     
-    return render(request, 'proprietes/unites/form_simple.html', context)
+    return render(request, 'proprietes/unites/form_etapes.html', context)
 
 
 @login_required
